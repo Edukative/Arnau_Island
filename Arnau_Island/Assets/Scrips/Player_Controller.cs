7cef97222c680f54de598d4cd9b17c969c3234a2
Edukative <56083723+Edukative@users.noreply.github.com>
2020-01-23 19:34:22 +0100

    Enemy Spawner
    We create enemy spawner

--- Arnau_Island/Assets/Scrips/Player_Controller.cs
+++ Arnau_Island/Assets/Scrips/Player_Controller.cs
@@ -6,17 +6,19 @@ public class Player_Controller : MonoBehaviour
 {
     private Rigidbody playerRB;
     public float speed;
+    GameObject focalPoint;
     
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint"); ;
     }
 
     // Update is called once per frame
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRB.AddForce(Vector3.forward * speed * forwardInput * Time.deltaTime);
+        playerRB.AddForce(focalPoint. transform. forward * speed * forwardInput * Time.deltaTime);
     }
 }
