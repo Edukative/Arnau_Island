2a8b57aaa599703afbbedb7845a838511b86a19c
Edukative <56083723+Edukative@users.noreply.github.com>
2020-01-30 19:33:05 +0100

    Add powerup
    add powerup

--- Arnau_Island/Assets/Scrips/Player_Controller.cs
+++ Arnau_Island/Assets/Scrips/Player_Controller.cs
@@ -6,7 +6,12 @@ public class Player_Controller : MonoBehaviour
 {
     private Rigidbody playerRB;
     public float speed;
+    private float horizontalInput;
+
     GameObject focalPoint;
+
+    bool hasPowerUp;
+    public float PowerUpStregth = 15;
     
     // Start is called before the first frame update
     void Start()
@@ -19,6 +24,34 @@ public class Player_Controller : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRB.AddForce(focalPoint. transform. forward * speed * forwardInput * Time.deltaTime);
+        playerRB.AddForce(focalPoint. transform. forward * speed * forwardInput * Time.deltaTime, ForceMode.Force);
+    }
+
+    IEnumerator PowerUpCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountdownRoutine());
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemtRB = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 pushAwayEnemy = (other.gameObject.transform.position - transform.position);
+            Debug.Log("Player collided with" + other.gameObject + "with powerup set to" + hasPowerUp);
+            enemtRB.AddForce(pushAwayEnemy * PowerUpStregth, ForceMode.Impulse);
+        }
     }
 }
--- Arnau_Island/Assets/Scrips/SpawnManager.cs
+++ Arnau_Island/Assets/Scrips/SpawnManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnMAnager : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
 
